1f0d838e4f7ef86912f7076e1055fd3a936d5dc4
Erick Gesser Schaeffer <erickgesserschaeffer1403@gmail.com>
2025-05-29 20:58:00 -0300

    Adicionar arquivos de projeto.

---
+++ ClubeDaLeitura.ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClubeDaLeitura.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Amigo amigo = new Amigo();
+        }
+    }
+
+    public class Amigo
+    {
+
+        public int id;
+
+    }
+}
