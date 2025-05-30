3227ef051f77570f2a00a4dc7608f06d8c5a795e
Erick Gesser Schaeffer <erickgesserschaeffer1403@gmail.com>
2025-05-30 14:10:05 -0300

    cadastrar amigo concluido

--- ClubeDaLeitura.ConsoleApp1/Program.cs
+++ ClubeDaLeitura.ConsoleApp1/Program.cs
@@ -1,17 +1,92 @@
-﻿namespace ClubeDaLeitura.ConsoleApp1
+﻿using System.Reflection.Metadata;
+
+namespace ClubeDaLeitura.ConsoleApp1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Amigo amigo = new Amigo();
+            TelaAmigo telaAmigo = new TelaAmigo();
+
+            while (true)
+            {
+                char opcaoEscolhida = telaAmigo.ApresentarMenu();
+
+                if (opcaoEscolhida == 'S')
+                    break;
+
+                switch (opcaoEscolhida)
+                {
+                    case '1':
+                        telaAmigo.CadastrarAmigo();
+                        break;
+
+                }
+            }
         }
-    }
 
-    public class Amigo
-    {
+        public class TelaAmigo
+        {
+            public char ApresentarMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Clube do Livro");
 
-        public int id;
+                Console.WriteLine();
 
+                Console.WriteLine("1 - Cadastrar Amigos");
+                Console.WriteLine("2 - Editar Amigos");
+                Console.WriteLine("3 - Excluir Amigos");
+                Console.WriteLine("4 - Visualizar Amigos");
+                Console.WriteLine("5 - Visualizar Empréstimos de Amigos");
+                Console.WriteLine("S - Sair");
+
+                Console.WriteLine();
+
+                Console.Write("Escolha uma Opção: ");
+                char opcaoEscolhida = Console.ReadLine().ToUpper()[0];
+
+                return opcaoEscolhida;
+            }
+
+            public void CadastrarAmigo()
+            {
+                Console.Clear();
+                Console.WriteLine("Clube do Livro");
+                Console.WriteLine();
+
+
+                Console.WriteLine("Cadastro de Amigos");
+
+                Console.Clear();
+                Console.WriteLine("Digite o nome do amigo: ");
+                string nomeAmigo = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("Digite o responsável: ");
+                string responsavelAmigo = Console.ReadLine();
+
+                Console.Clear();
+                Console.WriteLine("Digite o telefone: ");
+                string telefoneAmigo = Console.ReadLine();
+
+
+                Amigo amigo = new Amigo();
+                amigo.nome = nomeAmigo;
+                amigo.responsavel = responsavelAmigo;
+                amigo.telefone = telefoneAmigo;
+
+                Console.Clear();
+                Console.WriteLine($"\nAmigo \"{amigo.nome}\" cadastrado com exito!");
+                Console.ReadLine();
+            }
+        }
+
+        public class Amigo
+        {
+            public string nome;
+            public string responsavel;
+            public string telefone;
+        }
     }
 }
